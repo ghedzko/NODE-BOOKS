db151bf9864e606dce63b873ea644a333ab62128
ghedzko <ghedzko@gmail.com>
2021-04-28 19:08:44 -0300

    falta la navegación

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const{Router} =require("express");
 const router = Router();
 const fs = require("fs");
-const json_books = fs.readFileSync("src/books.json","utf-8")
-const books =JSON.parse(json_books);
+const { v4: uuidv4 } = require('uuid');
+const { get } = require("../app");
+const json_books = fs.readFileSync("src/books.json","utf-8");
+let books =JSON.parse(json_books);
 
 
 router.get("/",(req,res)=>{
@@ -20,10 +22,24 @@ router.post("/new-entry",(req,res)=>{
         res.status(400).send("Faltan ingresar datos");
         return
     }
+    let newBook={
+        id:uuidv4(),
+        title,
+        author,
+        image,
+        description
+    }
 
-    books.push(req.body);
+    books.push(newBook);
     const json_books = JSON.stringify(books);
     fs.writeFileSync("src/books.json",json_books,"utf-8");
-    res.send("Recibido");
+    res.redirect("/");
 });
+router.get("/delete/:id",(req,res)=>{
+   books= books.filter(book => book.id != req.params.id);
+   const json_books = JSON.stringify(books);
+   fs.writeFileSync("src/books.json",json_books,"utf-8");
+   res.redirect("/");
+})
+
 module.exports = router;
